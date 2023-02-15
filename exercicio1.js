9081ec4910fbb5aef1bb92ffb0544f103da2f310
vanessa ferreira <vanessa10623865silva@gmail.com>
2023-02-15 00:18:48 -0300

    ESTRUTURA DE CONDICAO, SE EU SOU DE MAIOR OU NAO.

---
+++ exercicio1.js
@@ -0,0 +1,13 @@
+
+/*
+estruturas de decisao caso seja de maior ou de menor
+*/
+var name = "Vanessa"
+var idade = 18
+if (idade <= 17) {
+    console.log("de menor");
+
+} else {
+
+    console.log("de maior");
+}
