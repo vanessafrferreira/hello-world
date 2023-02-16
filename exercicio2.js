c53cc05c0351b96b84a0bb8bac6edbdd83a3fdad
vanessa ferreira <vanessa10623865silva@gmail.com>
2023-02-16 00:29:53 -0300

    exercicio de soma e incremento

---
+++ exercicio2.js
@@ -0,0 +1,26 @@
+var total = 10 + 5 * 2 -1;
+console.log (total)
+
+var idade = "18s"
+var somaIdade = 7
+
+console.log (+idade + somaIdade)
+
+var idade2 = "x7"
+var somaIdade2 = 10
+
+console.log (+idade2 + somaIdade2)
+
+/* somando uma string */
+var incremento = "200"
+var incremento2 = 50
+
+console.log(+incremento + incremento2)
+
+var incremento = 5
+console.log(incremento++)
+console.log(incremento)
+
+/*como dividir o peso por 2*/
+var numero = 90
+var unidade = <k
